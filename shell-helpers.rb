067da8bb78d62289e851778762e98118089d95da
Philippe Desjardins <pdesjardins90@users.noreply.github.com>
2020-07-28 21:15:35 -0400

    chore: upgraded shell-helpers to 0.3.0

--- shell-helpers.rb
+++ shell-helpers.rb
@@ -2,8 +2,8 @@ class ShellHelpers < Formula
   desc "A collection of self-contained shell functions that simplify scripting"
   homepage "https://github.com/logisparte/shell-helpers"
   license "MIT"
-  url "https://github.com/logisparte/shell-helpers/archive/0.1.0.tar.gz"
-  sha256 "11ff661b86e9bf2299d07ac97699210653bcd11c6ef92d5f3e65743ee0c3198b"
+  url "https://github.com/logisparte/shell-helpers/archive/0.3.0.tar.gz"
+  sha256 "fce7df7afbb5e017053b7f121a0f4d863eddc2d841a6f31fd5985eea1bb712c9"
 
   def install
     bin.install "shell-helpers"
