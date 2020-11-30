a13dcc6d9eec01edb8b147562d91bb778311645a
helot <logisparte-helot@users.noreply.github.com>
2020-11-30 03:43:48 +0000

    chore(shell-helpers): released 0.5.11

--- shell-helpers.rb
+++ shell-helpers.rb
@@ -3,8 +3,8 @@
 class ShellHelpers < Formula
   desc "Collection of POSIX-compatible functions that simplify writing shell code"
   homepage "https://github.com/logisparte/shell-helpers"
-  url "https://packages.logisparte.com/shell-helpers/shell-helpers-0.5.7.tar.gz"
-  sha256 "f3ae389d4d61cb6a46a2364578064d93c1ab0e692636844c48962c90032ffa2a"
+  url "https://packages.logisparte.com/shell-helpers/shell-helpers-0.5.11.tar.gz"
+  sha256 "afbe0e6db77e74e88c4d4943d9df663e326b6eea2ec0b55b37f0c6b57c47f975"
   license "GPL-3.0-only"
 
   def install
