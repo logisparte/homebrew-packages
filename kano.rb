8d2de23c0d3af21f53fed592f9be51cc66d3c1de
Philippe Desjardins <pdesjardins90@users.noreply.github.com>
2021-02-24 23:14:58 -0500

    chore(kano): released 0.3.1

--- kano.rb
+++ kano.rb
@@ -3,21 +3,19 @@
 class Kano < Formula
   desc "Software engineering task management CLI"
   homepage "https://github.com/logisparte/kano"
-  url "https://packages.logisparte.com/kano/kano-0.2.5.tar.gz"
-  sha256 "0c699f5e11ba8b387fd2511f31cd37a5bbfdb1d98d457b47ce6532ab08ee04c5"
+  url "https://github.com/logisparte/kano/releases/download/0.3.1/kano.tar.gz"
+  sha256 "9d2641f90cc3c11cdc8aa10db21116cf7a06f336f5bb3ccb29133417598221be"
   license "GPL-3.0-only"
-  depends_on "logisparte/packages/shell-helpers"
 
   def install
-    libexec.install Dir["*"]
+    libexec.install Dir["libexec/*"]
+    doc.install Dir["share/doc/*"]
+    prefix.install "LICENSE"
     bin.write_exec_script (libexec/"kano")
   end
 
   test do
-    help_output = shell_output(
-      "export SHELL_HELPERS=\"$(brew --prefix)/opt/shell-helpers/libexec\" && kano help",
-    )
-
+    help_output = shell_output("kano help")
     assert_match /Show this help message/, help_output
   end
 end
