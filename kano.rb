342e93b3010b8c4e7d667d6f88b239b6caff394a
helot <logisparte-helot@users.noreply.github.com>
2021-10-14 22:18:23 +0000

    chore(kano): released 3.3.0

--- kano.rb
+++ kano.rb
@@ -3,8 +3,8 @@
 class Kano < Formula
   desc "Software engineering task management CLI"
   homepage "https://github.com/logisparte/kano"
-  url "https://github.com/logisparte/kano/releases/download/3.2.2/kano.tar.gz"
-  sha256 "52262be9347d65f4f711c77f3b64bf4cd7e5c8a06b337486d36986c24bbdd1d3"
+  url "https://github.com/logisparte/kano/releases/download/3.3.0/kano.tar.gz"
+  sha256 "3bd7b9651b523d3cf4c41315c50ac558938c778f184f1d35d9da0391345ee9c7"
   license "GPL-3.0-only"
   depends_on "docker" => :optional
 
