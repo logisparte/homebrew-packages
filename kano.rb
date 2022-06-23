db1c98a68178d4eb7f161b9ec86d7011dbe210b7
Philippe Desjardins <pdesjardins90@users.noreply.github.com>
2022-06-23 15:48:14 -0400

    test: wip

--- kano.rb
+++ kano.rb
@@ -7,7 +7,7 @@ class Kano < Formula
   sha256 "636a98be7eba78d99b085fb573caa13cac841693deec52fd6315062f3bd49819"
   license "GPL-3.0-only"
   depends_on "git" => :optional
-  depends_on cask: "docker" => :optional
+  caveats "Optional: brew install --cask docker"
 
   def install
     libexec.install Dir["libexec/*"]
