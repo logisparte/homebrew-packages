03efaaf4b0d7f0fb58c2e31196199d0db197ddb5
helot <logisparte-helot@users.noreply.github.com>
2022-06-23 20:46:01 +0000

    x(kano): released 3.6.1

--- kano.rb
+++ kano.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Kano < Formula
-  desc "Software engineering task management CLI"
+  desc "Software engineering workflow automation CLI"
   homepage "https://github.com/logisparte/kano"
-  url "https://github.com/logisparte/kano/releases/download/3.6.0/kano.tar.gz"
-  sha256 "636a98be7eba78d99b085fb573caa13cac841693deec52fd6315062f3bd49819"
+  url "https://github.com/logisparte/kano/releases/download/3.6.1/kano.tar.gz"
+  version "3.6.1"
+  sha256 "df95f7ad18fae6a716c429f6d0a48dae773f7335683a520d9b03ce2a8e2c5112"
   license "GPL-3.0-only"
-  depends_on "docker" => :optional
   depends_on "git" => :optional
 
   def install
@@ -14,6 +14,18 @@ class Kano < Formula
     doc.install Dir["share/doc/*"]
     prefix.install "LICENSE"
     bin.write_exec_script (libexec/"kano")
+  end
+
+  def caveats
+    <<~EOS
+      Homebrew formula does not support this syntax:
+        depends_on cask: "docker" => :optional
+
+      So, optionally do:
+        brew install --cask docker
+
+      To install Docker Desktop
+    EOS
   end
 
   test do
