fd6e7397100d4f0dfe398b1d6d205817bc40ced5
helot <logisparte-helot@users.noreply.github.com>
2022-06-24 02:29:37 +0000

    x(kano): released 3.6.2

--- kano.rb
+++ kano.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class Kano < Formula
-  desc "Software engineering task management CLI"
+  desc "Software engineering workflow automation CLI"
   homepage "https://github.com/logisparte/kano"
-  url "https://github.com/logisparte/kano/releases/download/3.6.0/kano.tar.gz"
-  sha256 "636a98be7eba78d99b085fb573caa13cac841693deec52fd6315062f3bd49819"
+  url "https://github.com/logisparte/kano/releases/download/3.6.2/kano.tar.gz"
+  
+  sha256 "7a70008a134f8758084c6bdccb565a9fa0a9db533df3c1ca1401d84a9aca8bce"
   license "GPL-3.0-only"
-  depends_on "docker" => :optional
   depends_on "git" => :optional
 
   def install
@@ -14,6 +14,16 @@ class Kano < Formula
     doc.install Dir["share/doc/*"]
     prefix.install "LICENSE"
     bin.write_exec_script (libexec/"kano")
+  end
+
+  def caveats
+    <<~EOF
+      Homebrew formula does not support this syntax:
+        depends_on cask: "docker" => :optional
+
+      So to (optionally) install Docker Desktop, do:
+        brew install --cask docker
+    EOF
   end
 
   test do
